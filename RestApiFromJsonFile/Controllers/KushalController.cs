bfe887a457c5f93c89c463a61584d66f73c4bb6d
kushal <kushaldeveloper@gmail.com>
2021-09-15 11:46:15 -0600

    Add project files.

---
+++ RestApiFromJsonFile/Controllers/KushalController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace RestApiFromJsonFile.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KushalController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public KushalController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<Kushal> Get()
+        {
+            _logger.LogInformation("Get Kushal");
+
+            string path = @"kushal.json";
+            Kushal kushal = new Kushal();
+            if (System.IO.File.Exists(path))
+            {
+                using StreamReader streamReader = new StreamReader(path);
+                string kushalText = await streamReader.ReadToEndAsync();
+                kushal = JsonConvert.DeserializeObject<Kushal>(kushalText);
+            }
+
+            return kushal;
+        }
+    }
+}
---
+++ RestApiFromJsonFile/Kushal.cs
@@ -0,0 +1,11 @@
+﻿namespace RestApiFromJsonFile
+{
+    public class Kushal
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+
+    }
+}
